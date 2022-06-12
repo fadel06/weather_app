8c4d78297cb962aa91d24f514d43887ba9a4fa66
fadel06 <fadelmuhammad06@gmail.com>
2022-06-12 13:32:40 +0800

    chore (route): create routes variable

---
+++ lib/common/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:weather_app/views/home_screen.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  HomeScreen.routeName: ((context) => const HomeScreen())
+};

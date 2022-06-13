ca3de2471a4d40044d075ecde26028297b370779
fadel06 <fadelmuhammad06@gmail.com>
2022-06-13 12:08:32 +0800

    chore (widgets): add padding left on label_text widget

--- lib/widgets/label_text.dart
+++ lib/widgets/label_text.dart
@@ -5,7 +5,7 @@ Padding labelText(
   String text,
 ) {
   return Padding(
-    padding: const EdgeInsets.only(bottom: 8.0, top: 16.0),
+    padding: const EdgeInsets.only(left: 16.0, bottom: 8.0, top: 16.0),
     child: Text(
       text,
       style: myTextTheme.subtitle1?.apply(color: kPrimaryDarkColor),
